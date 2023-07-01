dc8e5591e12c59602ec1ba8076c9d95aac5e8847
杰 <985341864@qq.com>
2023-07-01 13:23:58 +0800

    refactor: add cases about closure

--- gomock/calculation_test.go
+++ gomock/calculation_test.go
@@ -8,9 +8,34 @@ import (
 )
 
 func TestMockCalculation_Add(t *testing.T) {
-	ctrl := gomock.NewController(t)
-	mockCalculation := NewMockCalculation(ctrl)
-	mockCalculation.EXPECT().Add(gomock.Any(), gomock.Any()).Return(6).Times(1)
-	result := mockCalculation.Add(1, 2)
-	assert.Equal(t, 6, result)
+	t.Run("mock function", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		mockCalculation := NewMockcalculation(ctrl)
+		mockCalculation.EXPECT().add(gomock.Any(), gomock.Any()).Return(6).Times(1)
+		result := mockCalculation.add(1, 2)
+		assert.Equal(t, 6, result)
+	})
+	t.Run("mock function (uncovered closure)", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		mockCalculation := NewMockcalculation(ctrl)
+		mockCalculation.EXPECT().addFunc(gomock.Any(), gomock.Any(), gomock.Any()).Return(4)
+		e := &example{
+			c: mockCalculation,
+		}
+		result := e.executeAdd(1, 1)
+		assert.Equal(t, 4, result)
+	})
+	t.Run("mock function (covered closure)", func(t *testing.T) {
+		ctrl := gomock.NewController(t)
+		mockCalculation := NewMockcalculation(ctrl)
+		f := func(a, b int, f func(a, b int) int) int {
+			return f(a, b)
+		}
+		mockCalculation.EXPECT().addFunc(gomock.Any(), gomock.Any(), gomock.Any()).DoAndReturn(f)
+		e := &example{
+			c: mockCalculation,
+		}
+		result := e.executeAdd(1, 1)
+		assert.Equal(t, 2, result)
+	})
 }
--- gomock/mock_calculation.go
+++ gomock/mock_calculation.go
@@ -10,39 +10,53 @@ import (
 	gomock "github.com/golang/mock/gomock"
 )
 
-// MockCalculation is a mock of Calculation interface.
-type MockCalculation struct {
+// Mockcalculation is a mock of calculation interface.
+type Mockcalculation struct {
 	ctrl     *gomock.Controller
-	recorder *MockCalculationMockRecorder
+	recorder *MockcalculationMockRecorder
 }
 
-// MockCalculationMockRecorder is the mock recorder for MockCalculation.
-type MockCalculationMockRecorder struct {
-	mock *MockCalculation
+// MockcalculationMockRecorder is the mock recorder for Mockcalculation.
+type MockcalculationMockRecorder struct {
+	mock *Mockcalculation
 }
 
-// NewMockCalculation creates a new mock instance.
-func NewMockCalculation(ctrl *gomock.Controller) *MockCalculation {
-	mock := &MockCalculation{ctrl: ctrl}
-	mock.recorder = &MockCalculationMockRecorder{mock}
+// NewMockcalculation creates a new mock instance.
+func NewMockcalculation(ctrl *gomock.Controller) *Mockcalculation {
+	mock := &Mockcalculation{ctrl: ctrl}
+	mock.recorder = &MockcalculationMockRecorder{mock}
 	return mock
 }
 
 // EXPECT returns an object that allows the caller to indicate expected use.
-func (m *MockCalculation) EXPECT() *MockCalculationMockRecorder {
+func (m *Mockcalculation) EXPECT() *MockcalculationMockRecorder {
 	return m.recorder
 }
 
-// Add mocks base method.
-func (m *MockCalculation) Add(a, b int) int {
+// add mocks base method.
+func (m *Mockcalculation) add(a, b int) int {
 	m.ctrl.T.Helper()
-	ret := m.ctrl.Call(m, "Add", a, b)
+	ret := m.ctrl.Call(m, "add", a, b)
 	ret0, _ := ret[0].(int)
 	return ret0
 }
 
-// Add indicates an expected call of Add.
-func (mr *MockCalculationMockRecorder) Add(a, b interface{}) *gomock.Call {
+// add indicates an expected call of add.
+func (mr *MockcalculationMockRecorder) add(a, b interface{}) *gomock.Call {
 	mr.mock.ctrl.T.Helper()
-	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "Add", reflect.TypeOf((*MockCalculation)(nil).Add), a, b)
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "add", reflect.TypeOf((*Mockcalculation)(nil).add), a, b)
+}
+
+// addFunc mocks base method.
+func (m *Mockcalculation) addFunc(a, b int, f func(int, int) int) int {
+	m.ctrl.T.Helper()
+	ret := m.ctrl.Call(m, "addFunc", a, b, f)
+	ret0, _ := ret[0].(int)
+	return ret0
+}
+
+// addFunc indicates an expected call of addFunc.
+func (mr *MockcalculationMockRecorder) addFunc(a, b, f interface{}) *gomock.Call {
+	mr.mock.ctrl.T.Helper()
+	return mr.mock.ctrl.RecordCallWithMethodType(mr.mock, "addFunc", reflect.TypeOf((*Mockcalculation)(nil).addFunc), a, b, f)
 }
